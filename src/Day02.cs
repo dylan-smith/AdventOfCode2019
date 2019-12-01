3c6062db666c03974ec447620730fe88c74a1ef1
Dylan Smith <dysmith@microsoft.com>
2019-12-01 06:51:00 +0100

    blanked out Day 2

--- src/Day02.cs
+++ src/Day02.cs
@@ -7,25 +7,12 @@ namespace AdventOfCode
     {
         public static string PartOne(string input)
         {
-            return (input.Lines().Count(x => ContainsLetterCount(x, 2)) * input.Lines().Count(x => ContainsLetterCount(x, 3))).ToString();
-        }
-
-        private static bool ContainsLetterCount(string box, int count)
-        {
-            return box.Distinct().Any(l => box.Count(c => c == l) == count);
+            return "";
         }
 
         public static string PartTwo(string input)
         {
-            foreach (var c in input.Lines().ToList().GetCombinations(2))
-            {
-                if (c.First().Overlap(c.Last()).Length == c.First().Length - 1)
-                {
-                    return c.First().Overlap(c.Last());
-                }
-            }
-
-            throw new Exception();
+            return "";
         }
     }
 }
