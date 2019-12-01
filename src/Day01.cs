00d6313539e9b6e3e4b155132e13e88492cd9907
Dylan Smith <dysmith@microsoft.com>
2019-12-01 06:11:20 +0100

    cleaned up day 1 a bit

--- src/Day01.cs
+++ src/Day01.cs
@@ -8,26 +8,24 @@ namespace AdventOfCode
     {
         public static string PartOne(string input)
         {
-            return input.Integers().Sum(x => Math.Floor((double)x / 3.0) - 2).ToString();
+            return input.Doubles().Sum(x => Math.Floor(x / 3.0) - 2).ToString();
         }
 
         public static string PartTwo(string input)
         {
-            var modules = input.Integers();
+            var modules = input.Doubles();
             var total = 0.0;
 
             foreach (var m in modules)
             {
                 var fuel = Math.Floor((double)m / 3.0) - 2;
-                var moduleFuel = fuel;
+                total += fuel;
 
                 while (fuel > 0)
                 {
                     fuel = Math.Floor(fuel / 3) - 2;
-                    moduleFuel += fuel > 0 ? fuel : 0;
+                    total += fuel > 0 ? fuel : 0;
                 }
-
-                total += moduleFuel;
             }
 
             return total.ToString();
