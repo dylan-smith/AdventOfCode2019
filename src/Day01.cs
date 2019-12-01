7bb8d64f6aacdddcdbe335ffc3ecdd4aacb44a0b
Dylan Smith <dysmith@microsoft.com>
2019-12-01 06:08:51 +0100

    Day 2 solved

--- src/Day01.cs
+++ src/Day01.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode
@@ -7,12 +8,29 @@ namespace AdventOfCode
     {
         public static string PartOne(string input)
         {
-            return "foo";
+            return input.Integers().Sum(x => Math.Floor((double)x / 3.0) - 2).ToString();
         }
 
         public static string PartTwo(string input)
         {
-            return "";
+            var modules = input.Integers();
+            var total = 0.0;
+
+            foreach (var m in modules)
+            {
+                var fuel = Math.Floor((double)m / 3.0) - 2;
+                var moduleFuel = fuel;
+
+                while (fuel > 0)
+                {
+                    fuel = Math.Floor(fuel / 3) - 2;
+                    moduleFuel += fuel > 0 ? fuel : 0;
+                }
+
+                total += moduleFuel;
+            }
+
+            return total.ToString();
         }
     }
 }
