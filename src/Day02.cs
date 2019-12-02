bf03e2bfe3ea2c7df8d9d8433e71187f5739f7f2
Dylan Smith <dysmith@microsoft.com>
2019-12-02 06:19:31 +0100

    Refactored Day 2 - extracted RunProgram function

--- src/Day02.cs
+++ src/Day02.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace AdventOfCode
@@ -9,80 +10,58 @@ namespace AdventOfCode
         {
             var program = input.Integers().ToList();
 
-            var curIdx = 0;
-
             program[1] = 12;
             program[2] = 2;
-            
 
-            while (program[curIdx] != 99)
+            return RunProgram(program).ToString();
+        }
+
+        private static int RunProgram(List<int> program)
+        {
+            var ipc = 0;
+
+            while (program[ipc] != 99)
             {
-                var op = program[curIdx];
+                var op = program[ipc];
 
                 if (op == 1)
                 {
-                    var a = program[program[curIdx + 1]];
-                    var b = program[program[curIdx + 2]];
-                    var c = program[curIdx + 3];
+                    var a = program[program[ipc + 1]];
+                    var b = program[program[ipc + 2]];
+                    var c = program[ipc + 3];
 
                     program[c] = a + b;
                 }
 
                 if (op == 2)
                 {
-                    var a = program[program[curIdx + 1]];
-                    var b = program[program[curIdx + 2]];
-                    var c = program[curIdx + 3];
+                    var a = program[program[ipc + 1]];
+                    var b = program[program[ipc + 2]];
+                    var c = program[ipc + 3];
 
                     program[c] = a * b;
                 }
 
-                curIdx += 4;
+                ipc += 4;
             }
 
-            return program[0].ToString();
+            return program[0];
         }
 
         public static string PartTwo(string input)
         {
-            var program = input.Integers().ToList();
-            var backup = program.Select(x => x).ToList();
+            var backup = input.Integers().ToList();
 
             for (var x = 0; x <= 99; x++)
             {
                 for (var y = 0; y <= 99; y++)
                 {
-                    program = backup.Select(b => b).ToList();
+                    var program = backup.Select(b => b).ToList();
 
                     program[1] = x;
                     program[2] = y;
 
-                    var curIdx = 0;
-
-                    while (program[curIdx] != 99)
-                    {
-                        var op = program[curIdx];
-
-                        if (op == 1)
-                        {
-                            var a = program[program[curIdx + 1]];
-                            var b = program[program[curIdx + 2]];
-                            var c = program[curIdx + 3];
-
-                            program[c] = a + b;
-                        }
-
-                        if (op == 2)
-                        {
-                            var a = program[program[curIdx + 1]];
-                            var b = program[program[curIdx + 2]];
-                            var c = program[curIdx + 3];
-
-                            program[c] = a * b;
-                        }
-
-                        curIdx += 4;
-                    }
+                    RunProgram(program);
 
                     if (program[0] == 19690720)
                     {
