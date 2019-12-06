7fbe7a9c41cbe20f90d288e4170a4d7799442f09
Dylan Smith <dysmith@microsoft.com>
2019-12-06 09:00:08 +0100

    Day 6.1

--- src/Days/Day06.cs
+++ src/Days/Day06.cs
@@ -9,37 +9,41 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            //input = "X)B\nB)C\nC)D\nD)E\nE)F\nB)G\nG)H\nD)I\nE)J\nJ)K\nK)L";
+            var planets = BuildPlanetList(input);
 
-            var com = new Planet("COM");
-
-            var orbits = input.Lines().ToList();
-
-            while (orbits.Any())
-            {
-                AddOrbits(com, orbits);
-            }
-
-            return com.CountOrbits().ToString();
+            return planets.Sum(p => p.CountOrbits()).ToString();
         }
 
-        private void AddOrbits(Planet com, List<string> orbits)
+        private IEnumerable<Planet> BuildPlanetList(string input)
         {
-            var orbitsCopy = orbits.Select(x => x).ToList();
+            var lines = input.Lines().ToList();
+            var planets = new List<Planet>();
 
-            foreach (var x in orbitsCopy)
+            foreach (var line in lines)
             {
-                var left = x.Substring(0, 3);
-                var right = x.Substring(4);
+                var left = line.Substring(0, 3);
+                var right = line.Substring(4);
 
-                var planet = com.Find(left);
+                var leftPlanet = planets.SingleOrDefault(p => p.Name == left);
+                var rightPlanet = planets.SingleOrDefault(p => p.Name == right);
 
-                if (planet != null)
+                if (leftPlanet == null)
                 {
-                    planet.AddOrbit(right);
-                    orbits.Remove(x);
+                    leftPlanet = new Planet(left);
+                    planets.Add(leftPlanet);
                 }
+
+                if (rightPlanet == null)
+                {
+                    rightPlanet = new Planet(right);
+                    planets.Add(rightPlanet);
+                }
+
+                rightPlanet.Orbits = leftPlanet;
+                leftPlanet.Orbiters.Add(rightPlanet);
             }
+
+            return planets;
         }
 
         public override string PartTwo(string input)
@@ -49,7 +53,8 @@ namespace AdventOfCode.Days
 
         private class Planet
         {
-            private List<Planet> _orbits = new List<Planet>();
+            public List<Planet> Orbiters { get; set; } = new List<Planet>();
+            public Planet Orbits { get; set; }
             public string Name { get; set; }
 
             public Planet(string name)
@@ -57,54 +62,11 @@ namespace AdventOfCode.Days
                 Name = name;
             }
 
-            public void AddOrbit(string newPlanet)
-            {
-                var p = new Planet(newPlanet);
-
-                _orbits.Add(p);
-            }
-
             public int CountOrbits()
             {
-                var result = 0;
+                if (Orbits == null) return 0;
 
-                foreach (var p in _orbits)
-                {
-                    result += p.CountOrbits() + p.CountPlanets();
-                }
-
-                //System.Diagnostics.Debug.WriteLine($"{Name} = {result}");
-                return result;
-            }
-
-            private int CountPlanets()
-            {
-                var result = 1;
-
-                foreach (var p in _orbits)
-                {
-                    result += p.CountPlanets();
-                }
-
-                //System.Diagnostics.Debug.WriteLine($"{Name} = {result} - COUNT");
-                return result;
-            }
-
-            public Planet Find(string name)
-            {
-                if (Name == name) return this;
-
-                foreach (var x in _orbits)
-                {
-                    var result = x.Find(name);
-
-                    if (result != null)
-                    {
-                        return result;
-                    }
-                }
-
-                return null;
+                return Orbits.CountOrbits() + 1;
             }
         }
     }
