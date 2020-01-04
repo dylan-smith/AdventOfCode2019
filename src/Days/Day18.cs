9144d63a8c005a18f73c1e7a6208e84aa58480ec
Dylan Smith <dysmith@microsoft.com>
2020-01-04 17:56:59 -0600

    deleted obsolete code

--- src/Days/Day18.cs
+++ src/Days/Day18.cs
@@ -9,7 +9,6 @@ namespace AdventOfCode.Days
     public class Day18 : BaseDay
     {
         private int BEST = int.MaxValue;
-        private Dictionary<(Point a, Point b), (int distance, HashSet<Point> doors, HashSet<Point> keys)> _paths;
         private Dictionary<Point, Dictionary<Point, (int distance, HashSet<Point> doors, HashSet<Point> keys)>> _pathsByStart;
 
         public override string PartOne(string input)
@@ -19,16 +18,14 @@ namespace AdventOfCode.Days
             var startPos = GetStartPos(map);
             var keyMap = GetKeyMap(map);
 
-            //map = RestrictMap(map, keyMap, startPos);
-
-            _paths = GetPaths(map, keyMap, startPos);
+            var paths = GetPaths(map, keyMap, startPos);
 
             var keyPoints = keyMap.Select(k => k.Key).Append(startPos).ToList();
             _pathsByStart = new Dictionary<Point, Dictionary<Point, (int distance, HashSet<Point> doors, HashSet<Point> keys)>>();
 
             foreach (var key in keyPoints)
             {
-                var keyPaths = _paths.Where(p => p.Key.a == key).ToList();
+                var keyPaths = paths.Where(p => p.Key.a == key).ToList();
                 var keyDict = new Dictionary<Point, (int distance, HashSet<Point> doors, HashSet<Point> keys)>();
 
                 keyPaths.ForEach(p => keyDict.Add(p.Key.b, p.Value));
@@ -38,65 +35,9 @@ namespace AdventOfCode.Days
 
             Log(map.GetString());
 
-            //var paths = newMap.FindShortestPaths(c => c == '.', startPos).ToList();
-
-            //ImageHelper.CreateBitmap(newMap.GetUpperBound(0) + 1, newMap.GetUpperBound(1) + 1, @"C:\AdventOfCode\Day18New.bmp", (x,y) =>
-            //{
-            //    if (keyMap.Any(k => k.Key.X == x && k.Key.Y == y))
-            //    {
-            //        return Color.Red;
-            //    }
-
-            //    if (keyMap.Any(k => k.Value.X == x && k.Value.Y == y))
-            //    {
-            //        return Color.Blue;
-            //    }
-
-            //    if (newMap[x, y] == '#')
-            //    {
-            //        return Color.Black;
-            //    }
-
-            //    if (newMap[x,y] == '.')
-            //    {
-            //        if (paths.Any(p => p.Key.X == x && p.Key.Y == y && p.Value >= 0))
-            //        {
-            //            return Color.Pink;
-            //        }
-
-            //        return Color.White;
-            //    }
-
-            //    return Color.Beige;
-            //});
-
-            //FindPath(0, startPos, map, keyMap);
-            FindPath2(0, startPos, new HashSet<Point>(keyMap.Select(k => k.Key).ToList()));
+            FindPath(0, startPos, new HashSet<Point>(keyMap.Select(k => k.Key).ToList()));
 
             return BEST.ToString();
-        }
-
-        private char[,] RestrictMap(char[,] map, Dictionary<Point, Point> keyMap, Point startPos)
-        {
-            var newMap = map.Clone(c => c);
-
-            keyMap.ForEach(k => newMap[k.Value.X, k.Value.Y] = '.');
-
-            var keyPoints = keyMap.Select(k => k.Key).Append(startPos).ToList();
-
-            var points = GetValidPath(newMap, keyPoints);
-
-            foreach (var p in newMap.GetPoints().ToList())
-            {
-                if (!points.Any(z => z == p))
-                {
-                    newMap[p.X, p.Y] = '#';
-                }
-            }
-
-            keyMap.ForEach(k => newMap[k.Value.X, k.Value.Y] = '#');
-
-            return newMap;
         }
 
         private Dictionary<(Point a, Point b), (int distance, HashSet<Point> doors, HashSet<Point> keys)> GetPaths(char[,] map, Dictionary<Point, Point> keyMap, Point startPos)
@@ -108,8 +49,6 @@ namespace AdventOfCode.Days
             var keyPoints = keyMap.Select(k => k.Key).Append(startPos).ToList();
 
             var paths = GetPaths(newMap, keyPoints, keyMap);
-
-            //keyMap.ForEach(k => newMap[k.Value.X, k.Value.Y] = '#');
 
             return paths;
         }
@@ -125,19 +64,6 @@ namespace AdventOfCode.Days
                 var keys = path.Where(p => keyMap.Any(k => k.Key == p)).Where(p => p != combo.First() && p != combo.Last()).ToList();
 
                 result.Add((combo.First(), combo.Last()), (path.Count - 1, new HashSet<Point>(doors), new HashSet<Point>(keys)));
-            }
-
-            return result;
-        }
-
-        private HashSet<Point> GetValidPath(char[,] map, List<Point> keyPoints)
-        {
-            var result = new HashSet<Point>();
-
-            foreach (var combo in keyPoints.GetCombinations(2))
-            {
-                var path = GetShortestPath(map, combo.First(), combo.Last(), new HashSet<Point>());
-                result.AddRange(path);
             }
 
             return result;
@@ -184,40 +110,7 @@ namespace AdventOfCode.Days
             return result;
         }
 
-        //public void FindPath(int steps, Point pos, chargot [,] map, Dictionary<Point, Point> keys)
-        //{
-        //    if (!keys.Any())
-        //    {
-        //        if (steps < BEST)
-        //        {
-        //            BEST = steps;
-        //            Log(BEST.ToString());
-        //        }
-        //        return;
-        //    }
-
-        //    if (keys.Count == 23)
-        //    {
-        //        Log($"XXX - {steps}");
-        //    }
-
-        //    var paths = map.FindShortestPaths(c => c == '.', pos).Where(p => keys.ContainsKey(p.Key)).ToList();
-        //    paths.Sort((x, y) => x.Value.CompareTo(y.Value));
-
-        //    foreach (var p in paths)
-        //    {
-        //        var door = keys[p.Key];
-        //        map[door.X, door.Y] = '.';
-        //        keys.Remove(p.Key);
-
-        //        FindPath(steps + p.Value, p.Key, map, keys);
-
-        //        map[door.X, door.Y] = '#';
-        //        keys.Add(p.Key, door);
-        //    }
-        //}
-
-        public void FindPath2(int steps, Point pos, HashSet<Point> keys)
+        public void FindPath(int steps, Point pos, HashSet<Point> keys)
         {
             if (steps >= BEST) return;
 
@@ -236,18 +129,6 @@ namespace AdventOfCode.Days
                 Log($"{keys.Count} - {steps}");
             }
 
-            //var paths = _paths.Where(p => p.Key.a == pos)
-            //                  .Where(p => keys.Contains(p.Key.b))
-            //                  .Where(p => !p.Value.doors.Any(d => keys.Contains(d)))
-            //                  .OrderBy(p => p.Value.distance)
-            //                  .ToList();
-
-            //var paths = _paths.Where(p => p.Key.a == pos).ToList();
-            //var paths = _pathsByStart[pos].Where(p => keys.Contains(p.Key)).ToList();
-            //var paths = keys.Select(k => (dest: k, details: _pathsByStart[pos][k])).ToList();
-            //paths = paths.Where(p => !p.details.doors.Any(d => keys.Contains(d))).ToList();
-            //paths = paths.OrderBy(p => p.details.distance).ToList();
-
             var paths = keys.Select(k => (dest: k, details: _pathsByStart[pos][k]))
                             .Where(p => !p.details.doors.Any(d => keys.Contains(d)))
                             .OrderBy(p => p.details.distance)
@@ -259,7 +140,7 @@ namespace AdventOfCode.Days
                 var removedKeys = keys.Where(k => details.keys.Contains(k)).ToList();
                 removedKeys.ForEach(r => keys.Remove(r));
 
-                FindPath2(steps + details.distance, dest, keys);
+                FindPath(steps + details.distance, dest, keys);
 
                 keys.Add(dest);
                 keys.AddRange(removedKeys);
