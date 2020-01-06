446b629cf3e324650363609d5adca4146143178b
Dylan Smith <dysmith@microsoft.com>
2020-01-05 21:22:58 -0600

    added a Dequeue extension method that takes an IEnumerable

--- src/ExtensionMethods.cs
+++ src/ExtensionMethods.cs
@@ -1495,4 +1495,12 @@ namespace AdventOfCode
             }
         }
     }
+
+    public static class QueueExtensions
+    {
+        public static void Enqueue<T>(this Queue<T> q, IEnumerable<T> items)
+        {
+            items.ForEach(i => q.Enqueue(i));
+        }
+    }
 }
