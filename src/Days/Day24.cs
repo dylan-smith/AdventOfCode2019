e2b5e004c029877212e3a4379e8deb262ba5e818
Dylan Smith <dysmith@microsoft.com>
2020-01-08 23:05:56 -0600

    Day 24.1

--- src/Days/Day24.cs
+++ src/Days/Day24.cs
@@ -9,7 +9,48 @@ namespace AdventOfCode.Days
     {
         public override string PartOne(string input)
         {
-            throw new NotImplementedException();
+            var grid = input.CreateCharGrid();
+
+            var diversity = GetBiodiversityRating(grid);
+
+            var seen = new HashSet<int>();
+
+            while (!seen.Contains(diversity))
+            {
+                seen.Add(diversity);
+
+                grid = NextMinute(grid);
+                diversity = GetBiodiversityRating(grid);
+            }
+
+            return diversity.ToString();
+        }
+
+        private char[,] NextMinute(char[,] grid)
+        {
+            var result = grid.Clone(c => c);
+
+            foreach (var p in grid.GetPoints())
+            {
+                var bugCount = grid.GetNeighbors(p.X, p.Y, false).Count(c => c == '#');
+
+                if (grid[p.X, p.Y] == '#')
+                {
+                    result[p.X, p.Y] = bugCount == 1 ? '#' : '.';
+                }
+
+                if (grid[p.X, p.Y] == '.')
+                {
+                    result[p.X, p.Y] = bugCount == 1 || bugCount == 2 ? '#' : '.';
+                }
+            }
+
+            return result;
+        }
+
+        private int GetBiodiversityRating(char[,] grid)
+        {
+            return (int)grid.GetPoints('#').Sum(p => Math.Pow(2, (p.X) + (p.Y * 5)));
         }
 
         public override string PartTwo(string input)
