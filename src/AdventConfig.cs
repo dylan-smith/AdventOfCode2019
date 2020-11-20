0402b9af15f87738ce37e2564f5f545610d6ade7
Dylan Smith <dylan-smith@github.com>
2020-11-20 07:36:08 -0600

    Added testing framework

--- src/AdventConfig.cs
+++ src/AdventConfig.cs
@@ -6,6 +6,7 @@
         public static int DefaultDay = 24;
         public static string LogFileFolder = @"C:\git\AdventOfCode\logs";
         public static string InputFileFolder = @"C:\git\AdventOfCode2019\input";
+        public static string TestFileFolder = @"C:\git\AdventOfCode2019\tests";
         public static string SessionCookieFile = @"C:\AoC\AoCSessionCookie.txt";
         public static bool LogFilesEnabled = true;
     }
--- src/UI/MainWindowViewModel.cs
+++ src/UI/MainWindowViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 
@@ -17,11 +18,19 @@ namespace AdventOfCode
         public DayAttribute SelectedDay { get; set; }
         public string InputText { get; set; }
         public string OutputText { get; set; }
+        public string TestName { get; set; }
+        private DayAttribute _lastRunDay;
+        private string _lastRunInput;
+        private string _lastRunOutput;
+        private int _lastRunPart;
 
         public MainWindowViewModel()
         {
-            RunPartOneCommand = new RelayCommand(x => RunDay(1), x => CanRun());
-            RunPartTwoCommand = new RelayCommand(x => RunDay(2), x => CanRun());
+            RunPartOneCommand = new RelayCommand(async x => await RunDay(1), x => CanRun());
+            RunPartTwoCommand = new RelayCommand(async x => await RunDay(2), x => CanRun());
+            CreateTestCommand = new RelayCommand(x => CreateTest(), x => CanCreateTest());
+            RunAllTestsCommand = new RelayCommand(async x => await RunAllTests(), x => CanRunAllTests());
+            RunTodayTestsCommand = new RelayCommand(async x => await RunTodayTests(), x => CanRunTodayTests());
 
             Days = GetDayAttributes().ToList();
             SelectedDay = GetDefaultDay();
@@ -29,6 +38,9 @@ namespace AdventOfCode
 
         public ICommand RunPartOneCommand { get; set; }
         public ICommand RunPartTwoCommand { get; set; }
+        public ICommand CreateTestCommand { get; set; }
+        public ICommand RunAllTestsCommand { get; set; }
+        public ICommand RunTodayTestsCommand { get; set; }
 
         private DayAttribute GetDefaultDay()
         {
@@ -40,7 +52,162 @@ namespace AdventOfCode
             return SelectedDay != null;
         }
 
-        private void RunDay(int part)
+        private bool CanRunAllTests()
+        {
+            if (!Directory.Exists(AdventConfig.TestFileFolder))
+            {
+                return false;
+            }
+
+            var files = Directory.GetFiles(AdventConfig.TestFileFolder, "*.test");
+
+            return files.Any();
+        }
+
+        private bool CanRunTodayTests()
+        {
+            if (!Directory.Exists(AdventConfig.TestFileFolder))
+            {
+                return false;
+            }
+
+            if (SelectedDay == null)
+            {
+                return false;
+            }
+
+            var files = Directory.GetFiles(AdventConfig.TestFileFolder, $"{SelectedDay.Year}-{SelectedDay.Day}-*.test");
+
+            return files.Any();
+        }
+
+        private async Task RunAllTests()
+        {
+            StartNewTestLog();
+            LogMessage("=== Running All Tests ===");
+
+            await RunTests("*.test");
+        }
+
+        private async Task RunTodayTests()
+        {
+            StartNewTestLog();
+            LogMessage($"=== Running Tests For {SelectedDay.Year} Day {SelectedDay.Day} ===");
+
+            await RunTests($"{SelectedDay.Year}-{SelectedDay.Day}-*.test");
+        }
+
+        private async Task RunTests(string fileMatchPattern)
+        {
+            LogMessage($"Searching for tests in {AdventConfig.TestFileFolder}");
+
+            var tests = Directory.GetFiles(AdventConfig.TestFileFolder, fileMatchPattern);
+
+            LogMessage($"{tests.Length} tests found");
+            var passCount = 0;
+            var failCount = 0;
+
+            foreach (var test in tests)
+            {
+                var result = await RunTest(test);
+
+                if (result)
+                {
+                    passCount++;
+                }
+                else
+                {
+                    failCount++;
+                }
+            }
+
+            LogMessage($"{passCount} PASSED - {failCount} FAILED");
+            LogMessage("=== Test Run Complete ===");
+        }
+
+        private async Task<bool> RunTest(string testFile)
+        {
+            var contents = File.ReadAllText(testFile);
+            var fileName = Path.GetFileNameWithoutExtension(testFile);
+            var year = int.Parse(fileName.Split('-')[0]);
+            var day = int.Parse(fileName.Split('-')[1]);
+            var part = int.Parse(fileName.Split('-')[2]);
+            var testName = fileName.Substring(year.ToString().Length + day.ToString().Length + 4);
+
+            var split = contents.LastIndexOf(Environment.NewLine);
+
+            var input = contents.Substring(0, split);
+            var output = contents.Substring(split + Environment.NewLine.Length);
+
+            var runner = GetDayInstance(new DayAttribute(year, day));
+            runner.Log = NullLogger;
+
+            LogMessage($"Running Test: {year} Day {day} Part {part}: {testName}...");
+
+            var testTask = new Task<(string result, long elapsed)>(() => RunDay(input, runner, part));
+            testTask.Start();
+
+            // TODO: disable buttons and show spinner
+
+            await testTask;
+
+            if (testTask.Result.result == output)
+            {
+                LogMessage($"PASSED - Result: {testTask.Result.result} (Elapsed: {testTask.Result.elapsed})");
+                return true;
+            }
+            else
+            {
+                LogMessage($"FAILED - Expected Result: {output} - Actual Result: {testTask.Result.result} (Elapsed: {testTask.Result.elapsed})");
+                return false;
+            }
+        }
+
+        private bool CanCreateTest()
+        {
+            return !string.IsNullOrWhiteSpace(TestName) && _lastRunDay != null && !string.IsNullOrWhiteSpace(_lastRunInput) && !string.IsNullOrWhiteSpace(_lastRunOutput);
+        }
+
+        private void CreateTest()
+        {
+            if (!IsTestNameUnique(TestName, _lastRunDay, _lastRunPart))
+            {
+                LogMessage($"Create Test Failed, a test with the name {TestName} already exists for {SelectedDay.Year}.{SelectedDay.Day}");
+                return;
+            }
+
+            CreateTestFile(TestName, _lastRunDay, _lastRunPart, _lastRunInput, _lastRunOutput);
+        }
+
+        private bool IsTestNameUnique(string testName, DayAttribute testDay, int testPart)
+        {
+            var fileName = GetTestFileName(testName, testDay, testPart);
+
+            return !File.Exists(fileName);
+        }
+
+        private void CreateTestFile(string testName, DayAttribute testDay, int testPart, string input, string output)
+        {
+            var fileName = GetTestFileName(testName, testDay, testPart);
+
+            Directory.CreateDirectory(AdventConfig.TestFileFolder);
+            File.WriteAllText(fileName, CreateTestFileContents(input, output));
+
+            LogMessage($"Test Created: {fileName}");
+        }
+
+        private string GetTestFileName(string testName, DayAttribute testDay, int testPart)
+        {
+            var fileName = $"{testDay.Year}-{testDay.Day}-{testPart}-{testName}.test";
+            return Path.Combine(AdventConfig.TestFileFolder, fileName);
+        }
+
+        private string CreateTestFileContents(string input, string output)
+        {
+            return $"{input}{Environment.NewLine}{output}";
+        }
+
+        private async Task RunDay(int part)
         {
             StartNewLog(SelectedDay.Year, SelectedDay.Day);
 
@@ -52,54 +219,41 @@ namespace AdventOfCode
 
             var day = GetDayInstance(SelectedDay);
 
+            var dayTask = new Task<(string result, long elapsed)>(() => RunDay(input, day, part));
+            dayTask.Start();
+
             // TODO: disable buttons and show spinner
 
-            var dayThread = new DayThreadHarness(input, day, part, DayComplete);
-            var thread = new Thread(new ThreadStart(dayThread.Run));
-            thread.Start();
+            await dayTask;
+
+            LogMessage($"========= DONE (Elapsed: {dayTask.Result.elapsed}ms) =========");
+            LogMessage($"ANSWER: {dayTask.Result.result}");
+
+            Clipboard.SetText(dayTask.Result.result);
+
+            _lastRunDay = SelectedDay;
+            _lastRunPart = part;
+            _lastRunInput = input;
+            _lastRunOutput = dayTask.Result.result;
         }
 
-        private void DayComplete(string result, long elapsed)
+        private (string result, long elapsed) RunDay(string input, BaseDay runner, int part)
         {
-            LogMessage($"========= DONE (Elapsed: {elapsed}ms) =========");
-            LogMessage($"ANSWER: {result}");
+            string result;
+            var start = Stopwatch.StartNew();
 
-            Clipboard.SetText(result);
-        }
-
-        private class DayThreadHarness
-        {
-            private string _input;
-            private BaseDay _day;
-            private int _part;
-            private Action<string, long> _dayCompleteCallback;
-
-            public DayThreadHarness(string input, BaseDay day, int part, Action<string, long> dayCompleteCallback)
+            if (part == 1)
             {
-                _input = input;
-                _day = day;
-                _part = part;
-                _dayCompleteCallback = dayCompleteCallback;
+                result = runner.PartOne(input);
+            }
+            else
+            {
+                result = runner.PartTwo(input);
             }
 
-            public void Run()
-            {
-                string result;
-                var start = Stopwatch.StartNew();
+            var end = start.ElapsedMilliseconds;
 
-                if (_part == 1)
-                {
-                    result = _day.PartOne(_input);
-                }
-                else
-                {
-                    result = _day.PartTwo(_input);
-                }
-
-                var end = start.ElapsedMilliseconds;
-
-                Application.Current.Dispatcher.Invoke(() => _dayCompleteCallback(result, end));
-            }
+            return (result, end);
         }
 
         private string GetInput(DayAttribute day)
@@ -193,6 +347,10 @@ namespace AdventOfCode
             NotifyChange(nameof(OutputText));
         }
 
+        private void NullLogger(string message)
+        {
+        }
+
         private StreamWriter _logWriter;
 
         private void StartNewLog(int year, int day)
@@ -200,6 +358,27 @@ namespace AdventOfCode
             if (AdventConfig.LogFilesEnabled)
             {
                 var fileName = $"{DateTime.Now.ToString("yyyyMMddHHmmssffff")}-{year}.{day}.log";
+                var path = Path.Combine(AdventConfig.LogFileFolder, fileName);
+
+                if (_logWriter != null)
+                {
+                    _logWriter.Close();
+                }
+
+                Directory.CreateDirectory(AdventConfig.LogFileFolder);
+                _logWriter = File.CreateText(path);
+                _logWriter.AutoFlush = true;
+            }
+
+            OutputText = string.Empty;
+            NotifyChange(nameof(OutputText));
+        }
+
+        private void StartNewTestLog()
+        {
+            if (AdventConfig.LogFilesEnabled)
+            {
+                var fileName = $"{DateTime.Now.ToString("yyyyMMddHHmmssffff")}-Tests.log";
                 var path = Path.Combine(AdventConfig.LogFileFolder, fileName);
 
                 if (_logWriter != null)
